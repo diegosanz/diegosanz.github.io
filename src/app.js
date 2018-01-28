00109a87d91869b4f507a15452832352f483ea1c
Diego Sanz <diego2sanz@gmail.com>
2018-01-28 18:17:02 +0100

    ADD: support por redux dev tools

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import {createStore, combineReducers, applyMiddleware} from 'redux'
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
 import {Provider} from 'react-redux'
 
 import createHistory from 'history/createBrowserHistory'
@@ -22,11 +22,13 @@ const history = createHistory()
 // Build the middleware for intercepting and dispatching navigation actions
 const middleware = routerMiddleware(history)
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 // Add the reducer to your store on the `router` key
 // Also apply our middleware for navigating
 const store = createStore(
 	combineReducers({...reducers, router: routerReducer}),
-	applyMiddleware(middleware)
+	composeEnhancers(applyMiddleware(middleware))
 )
 
 // Now you can dispatch navigation actions from anywhere!
