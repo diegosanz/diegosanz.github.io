0190b5fb2c334ac2ea026b170d063d9e66ad6f58
Diego Sanz <diego2sanz@gmail.com>
2018-02-07 07:01:01 +0100

    ADD: bulma

--- src/app.js
+++ src/app.js
@@ -13,6 +13,8 @@ import reducers from './reducers'
 
 import router from './router/router'
 
+import bulma from 'bulma'
+
 // Create a history of your choosing (we're using a browser history in this case)
 const history = createHistory()
 
