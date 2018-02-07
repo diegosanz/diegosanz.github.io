67efe490ed86c7a1024541a62191cb933f6d504c
Diego Sanz <diego2sanz@gmail.com>
2018-02-07 10:38:13 +0100

    Add: 404 page

---
+++ src/components/error404.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import {Link} from 'react-router-dom';
+
+import Container from './container.js'
+import TopNavbar from './navbar.js'
+
+export default class Home extends Component {
+	render() {
+		return (
+			<Container>
+				<TopNavbar/>
+				<h1 className="title">Error 404</h1>
+			</Container>
+		)
+	}
+}
--- src/router/router.js
+++ src/router/router.js
@@ -1,18 +1,20 @@
 import React from "react";
-import {Route} from "react-router";
+import {Route, Switch} from "react-router";
 
 // Components
 import Home from './../components/home.js'
 import About from './../components/about.js'
 import Blog from './../components/blog.js'
+import Error404 from './../components/error404.js'
 
 function router() {
 	return (
-		<div>
+		<Switch>
 			<Route exact path = "/" component = { Home } />
 			<Route path = "/about" component = { About } />
 			<Route path = "/blog" component = { Blog } />
-		</div>
+			<Route component={ Error404 } />
+		</Switch>
 	);
 }
 export default router;
